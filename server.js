b3939773c6e762c0bb596c4436ed5f923c56ae1f
Aksel <anconda.2025@outlook.com>
2026-06-16 22:57:12 +0200

    Add Supabase integration

--- server.js
+++ server.js
@@ -100,7 +100,6 @@ app.post("/api/report-problem", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 myQaribi server su http://localhost:${PORT}`);
 });
